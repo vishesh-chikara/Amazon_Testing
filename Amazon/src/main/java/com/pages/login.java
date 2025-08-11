67b1b93fd09eaa86d9ae74da78a04c4fe87966d6
visheshkumar-self <visheshkumar0012@gmail.com>
2025-08-11 20:20:44 +0530

    First commit - "just to launch the Url"

---
+++ Amazon/src/main/java/com/pages/login.java
@@ -0,0 +1,31 @@
+package com.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class login {
+	 WebDriver driver;
+
+	    @BeforeClass
+	    public void setUp() {
+	        WebDriverManager.chromedriver().setup(); // Automatically downloads the driver
+	        driver = new ChromeDriver();
+	        driver.manage().window().maximize();
+	    }
+
+	    @Test
+	    public void openURL() throws InterruptedException {
+	        driver.get("https://www.amazon.in/");
+	        Thread.sleep(3000);
+	        System.out.println("Page title is: " + driver.getTitle());
+	    }
+
+	    @AfterClass
+	    public void tearDown() {
+	        driver.quit();
+	    }
+}
