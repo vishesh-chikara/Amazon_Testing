8521c5de50c3128b9a1341d19816fc6b6f1226fb
visheshkumar-self <visheshkumar0012@gmail.com>
2025-08-26 15:48:06 +0530

    Commit- 14 Script Update

--- Amazon/src/test/java/com/testcases/Test_Login.java
+++ Amazon/src/test/java/com/testcases/Test_Login.java
@@ -7,7 +7,7 @@ import com.pages.Login_Page;
 
 public class Test_Login extends Amazon_BaseClass {
 
-	@Test(priority = 4, description = "to test the login fucn with valid user and pass")
+	@Test(priority = 5, description = "to test the login fucn with valid user and pass")
 	public void Login_to_Amazon() throws InterruptedException
 
 	{
--- Amazon/src/test/java/com/testcases/Test_Suggestion.java
+++ Amazon/src/test/java/com/testcases/Test_Suggestion.java
@@ -1,14 +1,12 @@
 package com.testcases;
 
 import org.testng.annotations.Test;
-
 import com.BaseClass.Amazon_BaseClass;
-import com.pages.Search_field;
 import com.pages.Suggestion_Page;
 
 public class Test_Suggestion extends Amazon_BaseClass {
 	
-	@Test
+	@Test(priority = 4, description = "tO Verify the suggestions under search fields")
 	public void ValidateSuggestion() throws InterruptedException
 	{
 		Suggestion_Page S1 = new Suggestion_Page();
